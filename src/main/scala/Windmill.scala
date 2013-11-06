62cceafc1545f86042e561220a51918ddb12ea92
Thomas Rohée <roheethomas@gmail.com>
2013-11-05 19:11:49 -0500

    first commit

---
+++ src/main/scala/Windmill.scala
@@ -0,0 +1,10 @@
+/**
+ * User: thomas
+ * Date: 11/5/2013
+ */
+
+object Windmill {
+  def main(args: Array[String]) {
+    println("Hello, World!")
+  }
+}
---
+++ src/test/scala/WindmillSuite.scala
@@ -0,0 +1,9 @@
+/**
+ * User: thomas
+ * Date: 11/5/2013
+ */
+import org.scalatest.FunSuite
+
+class WindmillSuite extends FunSuite {
+
+}
