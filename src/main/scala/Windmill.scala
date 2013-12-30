07f04b7e19235334cafb5e57b4bf51aa6dcd0042
Thomas Rohée <roheethomas@gmail.com>
2013-12-30 11:46:49 -0500

    refactoring

--- src/main/scala/Windmill.scala
+++ src/main/scala/Windmill.scala
@@ -37,12 +37,11 @@ object Windmill extends App{
 
   val positions: Map[Int, Boolean] = (for {i <- 1 to 24} yield (i, false)).toMap
 
+  val board = new Board(positions)
   val player1 = new Player(List(), 9)
   val player2 = new Player(List(), 9)
 
-  var turnOf: Player = player1
-
-  val board = new Board(positions)
+  var turnOfP1: Boolean = true
   var gameState = new GameState(board, player1, player2)
 
   println("Round 1 will now begin. \n" +
@@ -51,26 +50,24 @@ object Windmill extends App{
     "the player can remove a pawn of its opponent. \n" +
     "The round ends when no pawn remains in players' hands.\n")
 
+  def updateGame(board: Board, player1: Player, player2: Player){
+    turnOfP1 = !turnOfP1
+    gameState = gameState.update(board, player1, player2)
+    round1(board, player1, player2)
+  }
+
   def round1(board: Board, player1: Player, player2: Player){
     println(gameState)
-    if (turnOf == player1) println("Turn of player 1.") else println("Turn of player 2.")
 
     if (player1.pawnsRem > 0 || player2.pawnsRem > 0){
+      if (turnOfP1) println("Turn of player 1.") else println("Turn of player 2.")
       print("Choose a position for you pawn: ")
 
       try{
         readInt() match {
-          case pos if board.availablePositions contains pos  =>
-            if (turnOf == player1){
-              if (player2.pawnsRem > 0) turnOf = player2 else print("No remaining pawns for player 2\n")
-              gameState = gameState.update(board.update(pos), player1.addPawn(pos), player2)
-              round1(board.update(pos), player1.addPawn(pos), player2)
-            }
-            else{
-              if (player1.pawnsRem > 0) turnOf = player1 else print("No remaining pawns for player 1\n")
-              gameState = gameState.update(board.update(pos), player1, player2.addPawn(pos))
-              round1(board.update(pos), player1, player2.addPawn(pos))
-            }
+          case pos if board.availablePositions contains pos =>
+            if (turnOfP1) updateGame(board.update(pos), player1.addPawn(pos), player2)
+            else updateGame(board.update(pos), player1, player2.addPawn(pos))
           case _ =>
             println("Position not available")
             round1(board: Board, player1, player2)
